71e0427c12ce8107f0887bed82e9d952b79dfa00
CAB2343 <gabrielalecab1@gmail.com>
2025-11-01 12:35:36 -0300

    COLETAVEIS

---
+++ Assets/_Scripts/Coletaveis.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coletaveis : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+        }
+    }
+
+
+
+
+
+
+
+}
