88471c459774108232812a12d5c3204cc2d01ed1
CAB2343 <gabrielalecab1@gmail.com>
2025-11-01 14:28:16 -0300

    dialogue

--- Assets/_Scripts/PlayerRayCast.cs
+++ Assets/_Scripts/PlayerRayCast.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class PlayerRayCast : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float rayDistance = 5f;
+    public float LugarDoRaio = 0.80f;
+    public GameObject DialogueText;
 
-    // Update is called once per frame
+
+
     void Update()
     {
-        
+        Vector3 RayOrigin = transform.position + Vector3.up * LugarDoRaio;
+        Ray ray = new Ray(RayOrigin, transform.forward);
+        RaycastHit hit;
+
+        Debug.DrawRay(RayOrigin, transform.forward * rayDistance, Color.red);
+
+        if (Physics.Raycast(ray, out hit, rayDistance))
+        {
+            if(hit.collider.CompareTag("Carlos"))
+            {
+                Debug.Log("Encostou em " + hit.collider.name);
+                Debug.DrawRay(RayOrigin, transform.forward * rayDistance, Color.green);
+                
+                if(Input.GetKeyDown(KeyCode.E))
+                {
+                    DialogueText.SetActive(true);
+                    Debug.Log("apertou");
+                }
+
+            }
+        }
     }
 }
