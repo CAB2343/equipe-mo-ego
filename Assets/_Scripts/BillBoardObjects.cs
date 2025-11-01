9129a49b887ab2c1b986e163e60b46a09dc71e98
CAB2343 <gabrielalecab1@gmail.com>
2025-11-01 15:57:55 -0300

    alteração player controller

--- Assets/_Scripts/BillBoardObjects.cs
+++ Assets/_Scripts/BillBoardObjects.cs
@@ -4,10 +4,39 @@ using UnityEngine;
 
 public class BillBoardObjects : MonoBehaviour
 {
+    public bool _enableAnyDirections = true;
+    public SpriteRenderer spriteRenderer;
+    public Sprite frontSprite;
+    public Sprite backSprite;
+    public Sprite leftSprite;
+    public Sprite rightSprite;
 
     void Update()
     {
+
         transform.LookAt(Camera.main.transform);
+
+        if(_enableAnyDirections == true)
+        {
+            Vector3 toCamera = Camera.main.transform.position - transform.position;
+            Vector3 localDirection = transform.InverseTransformDirection(toCamera.normalized);
+
+            if (localDirection.z > 0.5f)
+            {
+                spriteRenderer.sprite = frontSprite;
+            }
+            else if (localDirection.z < -0.5f)
+            {
+                spriteRenderer.sprite = backSprite;
+            }
+            else if (localDirection.x > 0.5f)
+            {
+                spriteRenderer.sprite = rightSprite;
+            }
+            else if (localDirection.x < -0.5f)
+            {
+                spriteRenderer.sprite = leftSprite;
+            }
+        }
     }
- 
 }
