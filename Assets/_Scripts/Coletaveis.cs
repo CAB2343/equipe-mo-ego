19081a5a9f45d641fe5552eb4a2f2b42d091e499
felipebassetto-debug <felipe.bassetto@uscsonline.com.br>
2025-11-02 02:54:09 -0300

    velocidade

--- Assets/_Scripts/Coletaveis.cs
+++ Assets/_Scripts/Coletaveis.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Coletaveis : MonoBehaviour
 {
     public UiManager ui;
+    public MulaController mula;
     void OnTriggerEnter(Collider other)
     {
         if(other.CompareTag("Player"))
@@ -13,11 +14,16 @@ public class Coletaveis : MonoBehaviour
             {
                 case "Ferradura":
                     ui.SomaFerradura();
+                    mula.chaseSpeed += 2f;
+                    mula.patrolSpeed += 1f;
                     break;
                 case "Corda":
                     ui.AlterCorda(true);
                     break;
             }
+            
+
+            
             Destroy(gameObject);
             Debug.Log("UUUU LEGAL VOCÊ PEGOU A PORRINHA");
         }
