c0c3d4e4678ddb9d7875d2448e1fa5b90f3972cc
felipebassetto-debug <felipe.bassetto@uscsonline.com.br>
2025-11-01 21:52:36 -0300

    Script UI

--- Assets/_Scripts/Coletaveis.cs
+++ Assets/_Scripts/Coletaveis.cs
@@ -4,10 +4,20 @@ using UnityEngine;
 
 public class Coletaveis : MonoBehaviour
 {
+    public UiManager ui;
     void OnTriggerEnter(Collider other)
     {
         if(other.CompareTag("Player"))
         {
+            switch (gameObject.tag)
+            {
+                case "Ferradura":
+                    ui.SomaFerradura();
+                    break;
+                case "Corda":
+                    ui.AlterCorda(true);
+                    break;
+            }
             Destroy(gameObject);
             Debug.Log("UUUU LEGAL VOCÊ PEGOU A PORRINHA");
         }
---
+++ Assets/_Scripts/UiManager.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UiManager : MonoBehaviour
+{
+    private int ferraduras = 0;
+    private bool corda = false;
+    public GameObject pngFerra;
+    public GameObject pngCorda;
+    public TextMeshProUGUI textFerra;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (ferraduras > 0)
+        {
+            pngFerra.SetActive(true);
+        }
+
+        if (corda)
+        {
+            pngCorda.SetActive(true);
+        }
+
+        if (ferraduras > 1)
+        {
+            textFerra.text = "" + ferraduras;
+        }
+    }
+    
+    public void AlterCorda(bool newCorda)
+    {
+        corda = newCorda;
+    }
+        
+    public void SomaFerradura()
+    {
+        ferraduras++;
+    }
+}
